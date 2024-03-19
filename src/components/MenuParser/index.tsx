8e55d405118cfd73bacf21e712867c1fb18e0941
dariovp <dariovp01@gmail.com>
2024-03-19 11:21:11 -0300

    fix: remove unused type check

--- src/components/MenuParser/index.tsx
+++ src/components/MenuParser/index.tsx
@@ -8,12 +8,6 @@ export function MenuParser(menu: { menu: MenuData }) {
 	return <>
 		{
 			menuData.map((item, i) => {
-				if (item.type == ItemType.Product) {
-					return <ProductItem key={i} data={item} />
-				}
-				if (item.type == ItemType.Table) {
-					return <Table key={i} data={item} />
-				}
 				return null
 			})
 		}

70a98453d003a05fe42cc29779d049fec1ffe2ea
dariovp <dariovp01@gmail.com>
2024-03-24 14:08:06 -0300

    feat: add final text & center screen

--- src/app/screen/page.tsx
+++ src/app/screen/page.tsx
@@ -13,17 +13,17 @@ export default function Home() {
 
 	useEffect(() => {
 		socket.on('data', (data: string) => {
-			setTranslation(data)
+			setTranslation(data.toLocaleUpperCase())
 		})
 	}, [socket]);
 
 
 	return (
-		<main className="flex flex-col justify-start items-center min-h-screen bg-yellow-100">
+		<main className="flex flex-col justify-center items-center min-h-screen bg-yellow-100">
 			<section className="flex flex-col justify-start items-center p-5 w-full">
-				<h2>
+				<h1>
 					{translation}
-				</h2>
+				</h1>
 			</section>
 		</main>
 	);
